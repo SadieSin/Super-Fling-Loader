20377781f6f0696349ee7ea07f4495f04adcb102
SadieSin <sarahnielsen2008@gmail.com>
2026-01-28 17:55:17 +0100

    SuperFlingX loader script

---
+++ loader.lua
@@ -0,0 +1,4 @@
+-- SuperFlingX Loader - by SadieSin
+-- Just paste this into your executor!
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SadieSin/Super-Fling-Loader/main/SuperFlingX.lua"))()
