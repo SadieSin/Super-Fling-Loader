dff07f7c2f2f8dc0facce51d0db82baea68b07da
SadieSin <sarahnielsen2008@gmail.com>
2026-01-28 18:11:19 +0100

    Delete loader.lua

--- loader.lua
+++
@@ -1,4 +0,0 @@
--- SuperFlingX Loader - by SadieSin
--- Just paste this into your executor!
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SadieSin/Super-Fling-Loader/main/SuperFlingX.lua"))()
